a2cd5145fa51dc4d5c1d545304ab3d28476ef621
Yasemin Tartici <yaztartici@gmail.com>
2019-05-18 09:29:28 -0400

    Start friends list

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [{
+    "name": "Jay",
+    "photo": "",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+    "name": "Tay",
+    "photo": "",
+    "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+},
+{
+    "name": "Ray",
+    "photo": "",
+    "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+},
+{
+    "name": "Bay",
+    "photo": "",
+    "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+},
+{
+    "name": "Kay",
+    "photo": "",
+    "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+},
+ 
+]
+
+
+module.exports = friends;
